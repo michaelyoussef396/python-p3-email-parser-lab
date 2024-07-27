401bf8fde33a3e1df183bbcd8ccf9247397d1062
michael youssef <michaelyoussef396@gmail.com>
2024-07-27 18:46:59 +1000

    completed tests

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,14 @@
-# your code goes here!
+# your code goes here!import re
+import re
+
+class EmailAddressParser:
+    def __init__(self, email_addresses):
+        self.email_addresses = email_addresses
+
+    def parse(self):
+        # Split the string by commas or spaces
+        addresses = re.split(r'[,\s]+', self.email_addresses)
+        # Remove any empty strings
+        addresses = [email for email in addresses if email]
+        # Return unique addresses, sorted alphabetically
+        return sorted(set(addresses))
